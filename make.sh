a6c652189fabd39a30c3a73b0210705b0cca42ef
Sergey Baturov <sergey@baturov.su>
2024-01-27 00:47:31 +0300

    Reorganization and addition of version control

--- make.sh
+++ make.sh
@@ -1,16 +1,53 @@
 #!/bin/sh
 
-P="$(dirname -- $0)"
-MKDEPLOY="$P/mkdeploy.sh"
+D="$(dirname -- $0)"
 
-SRCDIR="$P" \
-OUTDIR="$P" \
-INITFILE="install.sh" \
+# Choose 'mkdeploy'
+MKDEPLOY="$(command -v mkdeploy)"
+test -n "${MKDEPLOY}" || MKDEPLOY="${D}/mkdeploy.sh"
+test -x "${MKDEPLOY}" || MKDEPLOY="command -p sh \"${MKDEPLOY}\""
+
+# Set variables
+SRCDIR="${D}"
+OUTDIR="${D}"
+VERFILE="${D}/version"
+VERSION=""
+INITFILE="install.sh"
 FILELIST="
 bashrc
 exrc
 inputrc
 profile
 vimrc
-" \
-command -p sh "${MKDEPLOY}"
+"
+
+# Check version
+if test -f "${VERFILE}" ; then
+    # Get current version
+    VERSION="$(cat "${VERFILE}")"
+    if test -n "${VERSION}" ; then
+	_list=""
+	for _f in ${INITFILE} ${FILELIST} ; do
+	    test -n "${_list}" && _list="${_list} ${SRCDIR}/${_f}" || _list="${SRCDIR}/${_f}"
+	done
+	# Find newer files
+	if test -n "$(find ${_list} -newer "${VERFILE}" 2>/dev/null)" ; then
+	    # Bump version
+	    VERSION=$(( ${VERSION} + 1 ))
+	    echo "${VERSION}" > "${VERFILE}"
+	fi
+	unset _list _f
+    else
+	# Initial version
+	VERSION="1"
+	echo "${VERSION}" > "${VERFILE}"
+    fi
+fi
+
+# Run 'mkdeploy' passing parameters via variables
+SRCDIR="${SRCDIR}" \
+OUTDIR="${OUTDIR}" \
+VERSION="${VERSION}" \
+INITFILE="${INITFILE}" \
+FILELIST="${FILELIST}" \
+eval "${MKDEPLOY}"
