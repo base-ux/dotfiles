f1e4292ff354e5cb4485651806c19d7465de8f69
Sergey Baturov <sergey@baturov.su>
2024-01-29 11:47:25 +0300

    Simplify version control for just setting VERSION variable

--- make.sh
+++ make.sh
@@ -9,13 +9,12 @@ test -x "${MKDEPLOY}" || MKDEPLOY="command -p sh \"${MKDEPLOY}\""
 
 # Set variables
 PRODUCT="dotfiles"
-VERSION=""
+VERSION="2"
 
 SRCDIR="${D}"
 OUTDIR="${D}"
 
-OUTFILE="${OUTDIR}/${PRODUCT}.sh"
-VERFILE="${SRCDIR}/version"
+OUTFILE="${OUTDIR}/${PRODUCT}-v${VERSION}.sh"
 
 INITFILE="install.sh"
 FILELIST="
@@ -25,30 +24,6 @@ inputrc
 profile
 vimrc
 "
-
-# Check version
-if test -f "${VERFILE}" ; then
-    # Get current version
-    VERSION="$(cat "${VERFILE}")"
-    if test -n "${VERSION}" ; then
-	_list=""
-	for _f in ${INITFILE} ${FILELIST} ; do
-	    test -n "${_list}" && _list="${_list} ${SRCDIR}/${_f}" || _list="${SRCDIR}/${_f}"
-	done
-	# Find newer files
-	if test -n "$(find ${_list} -newer "${VERFILE}" 2>/dev/null)" ; then
-	    # Bump version
-	    VERSION=$(( ${VERSION} + 1 ))
-	    echo "${VERSION}" > "${VERFILE}"
-	fi
-	unset _list _f
-    else
-	# Initial version
-	VERSION="1"
-	echo "${VERSION}" > "${VERFILE}"
-    fi
-    OUTFILE="${OUTDIR}/${PRODUCT}-v${VERSION}.sh"
-fi
 
 # Run 'mkdeploy' passing parameters via variables
 SRCDIR="${SRCDIR}" \
